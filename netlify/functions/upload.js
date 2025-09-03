d3649cf9f02de31844c2b3feb3386be75537c4b3
Sef-Roercollege <Soml106998@soml.nl>
2025-09-03 19:32:34 +0200

    Update upload.js

--- netlify/functions/upload.js
+++ netlify/functions/upload.js
@@ -10,15 +10,15 @@ export async function handler(event) {
   const uploadPreset = process.env.CLOUDINARY_UPLOAD_PRESET;
 
   try {
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("upload_preset", uploadPreset);
+
     const response = await fetch(
       `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
       {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          file,
-          upload_preset: uploadPreset
-        })
+        body: formData
       }
     );
 
