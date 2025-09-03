cafb1edb03b495d184f7a6d63c9d9a704fc93b51
Sef-Roercollege <Soml106998@soml.nl>
2025-09-03 18:12:44 +0200

    Create upload.js

---
+++ netlify/functions/upload.js
@@ -0,0 +1,34 @@
+// netlify/functions/upload.js
+import fetch from "node-fetch";
+
+export async function handler(event) {
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+
+  const body = JSON.parse(event.body);
+  const { file } = body;
+
+  // Upload naar Cloudinary
+  const cloudName = process.env.CLOUDINARY_CLOUD_NAME;
+  const uploadPreset = process.env.CLOUDINARY_UPLOAD_PRESET;
+
+  const response = await fetch(
+    `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        file,
+        upload_preset: uploadPreset
+      })
+    }
+  );
+
+  const data = await response.json();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data)
+  };
+}
